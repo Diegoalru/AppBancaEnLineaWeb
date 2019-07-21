de9827930edbd7af9974caf859374698bd335b6c
Diego Rubi Salas <43016665+Diegoalru@users.noreply.github.com>
2019-07-21 12:57:16 -0600

    Nueva entidad transferencia.

---
+++ AppBancaEnLineaWeb/AppBancaEnLineaWeb/AppBancaEnLineaWeb/Models/Transferencia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBancaEnLineaWeb.Models
+{
+    public class Transferencia
+    {
+        #region Variables
+        public int TRA_CODIGO { get; set; }
+        public int CUE_ORIGEN { get; set; }
+        public int CUE_DSTINO { get; set; }
+        public string TRA_DESCRIPCION { get; set; }
+        public char TRA_ESTADO { get; set; }
+        public DateTime TRA_FECHA { get; set; }
+        public decimal TRA_MONTO { get; set; }
+        #endregion
+
+        #region Constructor
+        public Transferencia()
+        {
+        }
+        #endregion
+    }
+}
