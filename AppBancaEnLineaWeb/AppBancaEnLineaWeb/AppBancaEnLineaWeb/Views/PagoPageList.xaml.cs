0c8edbfb45a37d3b2c1452e3df4943d0989a5ddb
Diego Rubi Salas <43016665+Diegoalru@users.noreply.github.com>
2019-08-15 03:08:20 -0600

    Cambios Logica en PagoPageList. INESTABLE.

--- AppBancaEnLineaWeb/AppBancaEnLineaWeb/AppBancaEnLineaWeb/Views/PagoPageList.xaml.cs
+++ AppBancaEnLineaWeb/AppBancaEnLineaWeb/AppBancaEnLineaWeb/Views/PagoPageList.xaml.cs
@@ -7,19 +7,51 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using AppBancaEnLineaWeb.Controllers;
+using AppBancaEnLineaWeb.Models;
+using AppBancaEnLineaWeb.Views;
+
 namespace AppBancaEnLineaWeb.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class PagoPageList : ContentPage
-	{
-		public PagoPageList ()
-		{
-			InitializeComponent ();
-		}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PagoPageList : ContentPage
+    {
+        #region Variables
+        private PagoManager pagoManager = new PagoManager();
+        #endregion
 
+        #region Constructor
+        public PagoPageList()
+        {
+            InitializeComponent();
+            CargaDatos();
+        }
+        #endregion
+
+
+        #region Metodos
+        private async void CargaDatos()
+        {
+            try
+            {
+                //Obtenermos la lista de las cuentas del usuario.
+                IEnumerable<Pago> pagos = await pagoManager.ObtenerPagos(App.usuarioActual.USU_CODIGO.ToString());
+                PagoList.ItemsSource = pagos;
+                PagoList.BindingContext = pagos;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Boton para regresar al TabbedPage.
+        /// </summary>
         private void Btn_Regresar_Clicked(object sender, EventArgs e)
         {
-            Application.Current.MainPage = new MainPage();
+            Application.Current.MainPage = new MainTabbedPage();
         }
+        #endregion
     }
 }
