d35686d126e734c7f047963f39b7a2ebebf5f130
Gustavo Adolfo González Murillo <ggonzalezm216@ulacit.ed.cr>
2019-08-17 00:07:47 -0600

    Conversor

--- AppBancaEnLineaWeb/AppBancaEnLineaWeb/AppBancaEnLineaWeb/Utils/ConverMoneda.cs
+++ AppBancaEnLineaWeb/AppBancaEnLineaWeb/AppBancaEnLineaWeb/Utils/ConverMoneda.cs
@@ -14,9 +14,6 @@ namespace AppBancaEnLineaWeb.Utils
             string toCurrency1 = toCurrency;
             decimal amount1 = Convert.ToDecimal(amount);
 
-            // For other currency symbols see http://finance.yahoo.com/currency-converter/
-            // Construct URL to query the Yahoo! Finance API
-
             const string urlPattern = "http://finance.yahoo.com/d/quotes.csv?s={0}{1}=X&f=l1";
             string url = string.Format(urlPattern, fromCurrency1, toCurrency1);
 
