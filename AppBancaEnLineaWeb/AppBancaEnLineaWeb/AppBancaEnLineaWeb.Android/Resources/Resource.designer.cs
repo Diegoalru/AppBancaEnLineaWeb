89c116128db25a1a741a2b2f011a47eae10b0e41
Gustavo Adolfo González Murillo <ggonzalezm216@ulacit.ed.cr>
2019-08-17 02:38:47 -0600

    Resume

--- AppBancaEnLineaWeb/AppBancaEnLineaWeb/AppBancaEnLineaWeb.Android/Resources/Resource.designer.cs
+++ AppBancaEnLineaWeb/AppBancaEnLineaWeb/AppBancaEnLineaWeb.Android/Resources/Resource.designer.cs
@@ -4345,26 +4345,26 @@ namespace AppBancaEnLineaWeb.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020143
-			public const int avd_hide_password_1 = 2130837827;
-			
 			// aapt resource value: 0x7f020144
-			public const int avd_hide_password_2 = 2130837828;
+			public const int avd_hide_password_1 = 2130837828;
 			
 			// aapt resource value: 0x7f020145
-			public const int avd_hide_password_3 = 2130837829;
+			public const int avd_hide_password_2 = 2130837829;
+			
+			// aapt resource value: 0x7f020146
+			public const int avd_hide_password_3 = 2130837830;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020146
-			public const int avd_show_password_1 = 2130837830;
-			
 			// aapt resource value: 0x7f020147
-			public const int avd_show_password_2 = 2130837831;
+			public const int avd_show_password_1 = 2130837831;
 			
 			// aapt resource value: 0x7f020148
-			public const int avd_show_password_3 = 2130837832;
+			public const int avd_show_password_2 = 2130837832;
+			
+			// aapt resource value: 0x7f020149
+			public const int avd_show_password_3 = 2130837833;
 			
 			// aapt resource value: 0x7f020055
 			public const int ConfigureBlack = 2130837589;
@@ -5020,11 +5020,11 @@ namespace AppBancaEnLineaWeb.Droid
 			// aapt resource value: 0x7f02012e
 			public const int notification_icon_background = 2130837806;
 			
-			// aapt resource value: 0x7f020141
-			public const int notification_template_icon_bg = 2130837825;
-			
 			// aapt resource value: 0x7f020142
-			public const int notification_template_icon_low_bg = 2130837826;
+			public const int notification_template_icon_bg = 2130837826;
+			
+			// aapt resource value: 0x7f020143
+			public const int notification_template_icon_low_bg = 2130837827;
 			
 			// aapt resource value: 0x7f02012f
 			public const int notification_tile_bg = 2130837807;
@@ -5069,16 +5069,19 @@ namespace AppBancaEnLineaWeb.Droid
 			public const int round_swap_horizontal_circle_24 = 2130837820;
 			
 			// aapt resource value: 0x7f02013d
-			public const int tooltip_frame_dark = 2130837821;
+			public const int seguri = 2130837821;
 			
 			// aapt resource value: 0x7f02013e
-			public const int tooltip_frame_light = 2130837822;
+			public const int tooltip_frame_dark = 2130837822;
 			
 			// aapt resource value: 0x7f02013f
-			public const int TransferLogoWhite = 2130837823;
+			public const int tooltip_frame_light = 2130837823;
 			
 			// aapt resource value: 0x7f020140
-			public const int UlacitLogo = 2130837824;
+			public const int TransferLogoWhite = 2130837824;
+			
+			// aapt resource value: 0x7f020141
+			public const int UlacitLogo = 2130837825;
 			
 			static Drawable()
 			{
