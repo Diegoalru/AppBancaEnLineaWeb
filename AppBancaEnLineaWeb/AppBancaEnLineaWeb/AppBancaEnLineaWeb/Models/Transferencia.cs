cd73a0b9202c3f8456ebd9cea5e7fb19f759e465
Diego Rubi Salas <43016665+Diegoalru@users.noreply.github.com>
2019-08-17 14:49:32 -0600

    Cambio de las Variables usadas.

--- AppBancaEnLineaWeb/AppBancaEnLineaWeb/AppBancaEnLineaWeb/Models/Transferencia.cs
+++ AppBancaEnLineaWeb/AppBancaEnLineaWeb/AppBancaEnLineaWeb/Models/Transferencia.cs
@@ -8,22 +8,22 @@ namespace AppBancaEnLineaWeb.Models
     {
         #region Variables
         public int TRA_CODIGO { get; set; }
-        public int CUE_ORIGEN { get; set; }
-        public int CUE_DSTINO { get; set; }
+        public int TRA_CUENTA_ORIGEN { get; set; }
+        public int TRA_CUENTA_DESTINO { get; set; }
         public string TRA_DESCRIPCION { get; set; }
         public string TRA_ESTADO { get; set; }
         public DateTime TRA_FECHA { get; set; }
-        public decimal TRA_MONTO_ORIGEN { get; set; }
-        public decimal TRA_MONTO_FINAL { get; set; }
+        public decimal TRA_MONTO { get; set; }
+
         public string TRA_DESCRIPCION_label
         {
             get
             {
                 return string.Format("Transferencia #{0} | Cuenta Origen #{1} | Cuenta Destino: #{2} | Monto: {3:N2} | Fecha: {4}"
                 , TRA_CODIGO
-                , CUE_ORIGEN
-                , CUE_DSTINO
-                , TRA_MONTO_FINAL
+                , TRA_CUENTA_ORIGEN
+                , TRA_CUENTA_DESTINO
+                , TRA_MONTO
                 , TRA_FECHA.ToString("dd/MM/yyyy"));
             }
         }
